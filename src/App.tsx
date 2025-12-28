2dd58e8c7ebc70d3f0fffc8472e17f9a5d4e28e5
FlowerCostPro <pcflorist@columbus.rr.com>
2025-12-27 23:11:50 -0500

    Added index.html

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>FlowerCostPro</h1>
+        <p>Welcome to your application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
